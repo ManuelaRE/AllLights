efb20087631fdc7511dd05a92a5658aa00b2de74
Manuela Redinciuc <46057566+ManuelaRE@users.noreply.github.com>
2020-12-11 15:37:28 +0000

    added pop-up notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.module.scss';
 import Routes from './containers/routes';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { UserProvider } from "./context/userContext";
 
 import inventory from './data/inventory';
 
+toast.configure();
 function App() {
 
   const [count, setCount] = useState(0);
@@ -18,11 +20,13 @@ function App() {
       product.quantity = quantity;
       setCart([...cart, product])
     }
+    toast("Item added to the basket!")
   }
 
 
   return (
     <UserProvider>
+      <ToastContainer />
       <div className="App">
         <Routes inventory={inventory} addToBasket={addToBasket} cart={cart} setCount={setCount} />
       </div>
